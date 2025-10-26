016296d755aff3111d73015b4f707b0cacbdf947
fahadkhan030 <fahdkhn30@gmail.com>
2025-10-26 11:32:04 +0500

    Build Todo App using useState for state management Focused on mastering useState for effective state handling Improved UI and interactivity using React and Tailwind CSS Wrote clean, readable, and beginner-friendly code. Implemented add and delete todo functionalities

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,10 @@
-import React, { useContext, useRef, useEffect, useReducer } from "react";
+import React, {
+  useContext,
+  useRef,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import UserContext from "../UserContext.jsx";
 // import { act } from "react";
 
@@ -17,28 +23,72 @@ function reducer(state, action) {
 
 const Header = () => {
   const { user } = useContext(UserContext);
+  // const [disable, setDisable] = useState(false);
   const [state, dispatch] = useReducer(reducer, initialState);
   const auto = useRef(null);
+  const [input, setInput] = useState("");
+  const [value, setValue] = useState([]);
 
   useEffect(() => {
     auto.current.focus();
-    console.log("wow its work");
   }, []);
+
+  const change = (event) => {
+    setInput(event.target.value);
+  };
+
+  const Click = () => {
+    setValue([...value, input]);
+    setInput("");
+  };
+
+  const remove = (indextoRemove) => {
+    setValue(value.filter((_, index) => indextoRemove !== index));
+  };
 
   return (
     <div className=" flex items-center justify-center flex-col gap-10 text-xl font-semibold">
       <span>Header — Welcome {user}</span>
-      <input
-        type="text"
-        className="border border-black p-1 rounded-2xl"
-        ref={auto}
-        placeholder="type Something.."
-      />
+      <div className="flex flex-col gap-3">
+        <div className="flex  gap-2">
+          <input
+            type="text"
+            className="border border-black p-2   rounded-2xl"
+            onChange={change}
+            value={input}
+            ref={auto}
+            placeholder="type Something.."
+          />
+          <button
+            onClick={Click}
+            className="border bg-green-300 border-black rounded-sm p-1"
+          >
+            add
+          </button>
+        </div>
+        <div className=" flex flex-col  items-center justify-between px-2 py-1 gap-2 rounded-xl">
+          {value.map((val, index) => (
+            <h1
+              key={index}
+              className="flex gap-2 bg-gray-400  rounded-2xl w-full   justify-between"
+            >
+              <span className="mx-2">.{val}</span>
+              <button
+                className="bg-amber-50 px-2 rounded-2xl"
+                onClick={() => remove(index)}
+              >
+                R
+              </button>
+            </h1>
+          ))}
+        </div>
+      </div>
 
       <div className="flex gap-2 ">
         <button
           className="border  border-red-500 rounded-xl shadow-2xl shadow-black p-1"
           onClick={() => dispatch({ type: "subtract" })}
+          disabled={state === 0}
         >
           -
         </button>
@@ -49,6 +99,7 @@ const Header = () => {
         >
           +
         </button>
+        <button onClick={() => dispatch({ type: "reset" })}>rest</button>
       </div>
     </div>
   );
