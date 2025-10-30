a288c9da46f401cfe2e5ac98183a21611a7142f4
fahadkhan030 <fahdkhn30@gmail.com>
2025-10-30 14:39:11 +0500

    Implement todo functionality in Footer component with input handling

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,16 +1,34 @@
-import React, { useReducer } from "react";
-
-const todos = [{}];
+import React, { useReducer, useState } from "react";
 
 function todoReducer(state, action) {
   switch (action.type) {
     case "add":
-      return [];
+      return [...state, { id: Date.now(), text: action.payload }];
+    case "remove":
+      return state.filter((todo) => todo.id !== action.payloads);
   }
 }
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [state, dispatch] = useReducer(todoReducer, []);
+  const [inputValue, setInputValue] = useState("");
+
+  const timer = setTimeout(() => {
+    console.log(inputValue);
+  }, 1000);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      Footer
+      <input
+        type="text"
+        className="border border-orange-400 rounded-sm"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <button onClick={}>ADD</button>
+    </div>
+  );
 };
 
 export default Footer;
